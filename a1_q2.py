0bdd5d23257dfb7da1ed2fde218882a188c0d6e0
Payal Narwal <122198919+PayalNarwal@users.noreply.github.com>
2023-01-15 13:39:45 +0530

    Create a1_q2.py

---
+++ a1_q2.py
@@ -0,0 +1,11 @@
+marks = int(input())
+if marks>90:
+    print("excellent")
+elif marks>80 and marks<=90:
+    print("good")
+elif marks>70 and marks<=80:
+    print("fair")
+elif marks>60 and marks<=70:
+    print("meets expectations")
+else:
+    print("below par")
