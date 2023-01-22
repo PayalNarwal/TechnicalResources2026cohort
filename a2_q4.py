06c3cddd733740cafea2d081914b08026aebaf23
Payal Narwal <122198919+PayalNarwal@users.noreply.github.com>
2023-01-22 14:40:04 +0530

    Create a2_q4.py

---
+++ a2_q4.py
@@ -0,0 +1,8 @@
+n = int(input())
+if 1 <= n <= 100 :
+    for i in range(1,n+1):
+        for j in range(1,i):
+            print("\t",end="")
+        for k in range(1,n-i+2):
+            print("*",end="\t")
+        print()
