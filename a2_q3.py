7329a18559da57ed096b342d57fec8a80dc2cecc
Payal Narwal <122198919+PayalNarwal@users.noreply.github.com>
2023-01-22 14:39:30 +0530

    Create a2_q3.py

---
+++ a2_q3.py
@@ -0,0 +1,8 @@
+n = int(input())
+if 1 <= n <= 100 :
+    for i in range(1,n+1):
+        for j in range(1,n-i+1):
+            print("\t",end="")
+        for k in range(1,i+1):
+            print("*",end="\t")
+        print()
